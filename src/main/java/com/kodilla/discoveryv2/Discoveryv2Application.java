42c766a5d207d2f580d382dead23c9716e8edaa5
unknown <mateuszp.nowak@gmail.com>
2023-01-17 08:20:10 +0100

    Initial commit

---
+++ src/main/java/com/kodilla/discoveryv2/Discoveryv2Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.discoveryv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Discoveryv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Discoveryv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/discoveryv2/Discoveryv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.discoveryv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Discoveryv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
